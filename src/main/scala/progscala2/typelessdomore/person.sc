e98941e3ff9c4d649c3a14d00225412345f044a1
aliaksandr.kachurka <Aliaksandr_Kachurka@epam.com>
2016-02-09 11:51:28 +0300

    Multiple lists of arguments example

---
+++ src/main/scala/progscala2/typelessdomore/person.sc
@@ -0,0 +1,11 @@
+class Person(val name: String, var age: Int)
+
+val p = new Person("Sasha Kochurko", 19)
+
+p.name
+
+p.age
+
+// p.name = "Sasha Guk" - error
+
+p.age = 20
---
+++ src/main/scala/progscala2/typelessdomore/shapes/Shapes.scala
@@ -0,0 +1,18 @@
+package progscala2.typelessdomore.shapes
+
+/**
+  * Created by Aliaksandr_Kachurka on 2/9/2016.
+  */
+case class Point(x: Double = 0.0, y: Double = 0.0) {
+  def shift(deltaX: Double = 0.0, deltaY: Double = 0.0) =
+    copy(x + deltaX, y + deltaY)
+}
+
+abstract class Shape() {
+  def draw(offset: Point = Point())(f: String => Unit): Unit =
+    f(s"draw(offset = $offset), ${this.toString}")
+}
+
+case class Circle(center: Point, radius: Double) extends Shape
+
+case class Rectangle(lowerLeft: Point, height: Double, width: Double) extends Shape
